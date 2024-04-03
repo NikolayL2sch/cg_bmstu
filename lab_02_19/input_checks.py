142e89e61307ab1d54dd8f02803a933e7b8f64de
Nikolay Milenko <nikolay.milenko@gmail.com>
2024-04-03 14:31:19 +0300

    lab_02 done

--- lab_02_19/input_checks.py
+++ lab_02_19/input_checks.py
@@ -5,6 +5,7 @@ from dialogs import show_err_win
 def check_radius(radius: float) -> bool:
     if radius > 0:
         return True
+    show_err_win("Значение радиуса должно быть больше 0!")
     return False
 
 
@@ -12,8 +13,16 @@ def params_to_float(*args: List[str]) -> List[float]:
     float_args = []
     if '' in args:
         show_err_win("Не введены все параметры для совершения этого действия")
+        return float_args
     try:
         float_args = [float(arg) for arg in args]
     except ValueError:
         show_err_win("Введены некорректные символы")
     return float_args
+
+
+def check_scale_koeff(scale_koeff: float) -> bool:
+    if abs(scale_koeff) > 1e-13:
+        return True
+    show_err_win("Значение коэффициента не должно быть равно 0!")
+    return False
--- lab_02_19/matrix_methods.py
+++ lab_02_19/matrix_methods.py
@@ -5,7 +5,7 @@ from class_point import Point
 
 def get_new_coords(matrix: List[List[Union[float, int]]], points: List[Point]) -> List[List[Union[float, int]]]:
     for _ in range(len(points)):
-        points[_] = mul_matrices(points[_], matrix)
+        points[_] = mul_matrices(matrix, points[_])
     return points
 
 
@@ -15,4 +15,5 @@ def mul_matrices(matrix: List[List[Union[float, int]]], point: Point):
     for i in range(len(matrix)):
         for k in range(len(point_matrix)):
             result[i] += matrix[i][k] * point_matrix[k]
-    return result
+    result.pop()
+    return Point(*result)
